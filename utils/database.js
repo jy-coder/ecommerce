cbec15bbbebd41de2172cdbfd388423df01e3ae2
JY <cjyclx@gmail.com>
2020-08-06 00:44:06 +0800

    test

--- utils/database.js
+++ utils/database.js
@@ -4,6 +4,7 @@ const Sequelize = require('sequelize');
 sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   protocol: 'postgres',
+  rejectUnauthorized: false,
   dialectOptions: {
       ssl: true
   }
