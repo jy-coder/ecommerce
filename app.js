892b6682b36e72682257772f63eafe566e295ec9
JY <cjyclx@gmail.com>
2020-08-06 02:24:04 +0800

    test deployment

--- app.js
+++ app.js
@@ -45,13 +45,6 @@ app.use(cors({
     
 }))
 
-if(process.env.NODE_ENV === 'production'){
-  //set static folder
-  app.use(express.static('client/build'));
-}
-app.get('*',(req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
 
 app.use(compression())
 
@@ -89,6 +82,16 @@ const port = process.env.PORT || 1337;
 app.use('/api/shop', shopRoute)
 app.use('/api/admin', adminRoute)
 app.use('/api/user', userRoute)
+
+
+if(process.env.NODE_ENV === 'production'){
+  //set static folder
+  app.use(express.static('client/build'));
+}
+app.get('*',(req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
+
 
 //Handing Unhandled Routes
 app.all('*', (req, res, next) => {
