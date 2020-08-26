9fd5c162254616fb18bb977bb1cb02e937374b18
JY <cjyclx@gmail.com>
2020-08-26 13:48:06 +0800

    test cors modified

--- app.js
+++ app.js
@@ -68,7 +68,14 @@ app.use('/api/admin', adminRoute)
 app.use('/api/user', userRoute)
 
 
+if(process.env.NODE_ENV === 'production'){
+  //set static folder
+  app.use(express.static('client/build'));
 
+  app.get('*',(req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 
 
@@ -103,14 +110,7 @@ app.listen(port,() =>{
     });
 app.use(globalErrorHandler)
 
-if(process.env.NODE_ENV === 'production'){
-  //set static folder
-  app.use(express.static('client/build'));
 
-  app.get('*',(req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
 
 
 
