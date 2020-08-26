190855fdb2446381412ce51dbab838c1010437fd
JY <cjyclx@gmail.com>
2020-08-26 14:47:09 +0800

    test cors modified

--- client/src/utils/axios-handler.js
+++ client/src/utils/axios-handler.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const instance = axios.create({
     // baseURL: 'http://127.0.0.1:1337/api'
-    baseURL: 'https://mern-shop-ecommerce.herokuapp.com/api'
+    baseURL: 'https://pern-shop-ecommerce.herokuapp.com/api'
 })
 
 
